299532f7df887c55ccc9b3d74e5be3a731730ae3
JKarthik177 <jkarthik177@gmail.com>
2018-12-20 12:31:35 +0530

    Add files via upload
    Recent

---
+++ Scratch book.py
@@ -0,0 +1,16 @@
+class Karthik:
+
+    def __init__(self,color,height,place):
+        self.color = color
+        self.height = height
+        self.place = place
+
+p1 = Karthik("browm",5.11,"Chennai")
+p2 = Karthik("black",6,"bangalore")
+print(p1.color)
+print(p1.height)
+print(p1.place)
+
+print(p2.color)
+print(p2.height)
+print(p2.place)
